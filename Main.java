ed134a447d1ddef0d48d4d8b2a03bde07579dc1b
Pavel Vokaty <148907163+PavelVokaty@users.noreply.github.com>
2024-02-06 17:20:31 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -20,9 +20,11 @@ public class Peroutka {
 
         System.out.println("\n- PEROUTKOVA KALKULAČKA HERMESU -\n");
 
+        // Vstup uživatele
         System.out.println("Zadejte počet znaků ve Wordu nebo řádků v Hermesu:");
         int a = scanner.nextInt();
 
+        // Výpočty řádků, znaků a otázek s odpověďmi
         int pocetRadku = a / 29;                        // 1 řádek = 29 znaků
         float pocetZnaku = a * 29;
         int pocetRadkuOrientace = a / 35;               // 1 řádek = 29 znaků
@@ -31,6 +33,7 @@ public class Peroutka {
         int pocetOtazekZnaku = a / 608;                 // 1 otázka s odpovědí = 608 znaků
         int pocetOtazekRadku = a / 24;                  // 1 otázka s odpovědí = 24 řádků
 
+        // Výpis přepočtených řádků, znaků a otázek s odpověďmi
         System.out.println("\nZNAKY");
         System.out.println("- MS WORD - " + a + " znaků je " + pocetRadku + " řádků v Hermesu");
         System.out.println("- ROZHOVOR - " + a + " znaků je " + pocetOtazekZnaku + " otázek s odpověďmi (+ extra perex na 240 znaků)");
