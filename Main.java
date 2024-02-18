26ed5605f67c0121f01597f7785174f44ac28544
Pavel Vokaty <148907163+PavelVokaty@users.noreply.github.com>
2024-02-18 14:06:22 +0100

    Update Main.java
    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 /**
- * Rychlá kalkulačka pro přepočet novinových řádků na znaky a opačně v redakčním systému Hermes Lidových novin
+ * Rychlá kalkulačka pro přepočet novinových řádků na znaky a opačně v redakčním systému Hermes pro různé layouty rubrik Lidových novin
 */
 
 import java.util.Scanner;
@@ -24,15 +24,17 @@ public class Peroutka {
         int pocetOtazekZnaku = a / 608;
         int pocetOtazekRadku = a / 24;
 
-        // Výpis přepočtených řádků, znaků a otázek s odpověďmi
+        // Výpis přepočtených znaků a otázek s odpověďmi pro domácí, ekononomickou, zahraniční a kulturní rubriku
         System.out.println("\nZNAKY");
         System.out.println("- MS WORD - " + a + " znaků je " + pocetRadku + " řádků v Hermesu");
         System.out.println("- ROZHOVOR - " + a + " znaků je " + pocetOtazekZnaku + " otázek s odpověďmi (+ extra perex na 240 znaků)");
 
+        // Výpis přepočtených řádků a otázek s odpověďmi pro domácí, ekononomickou, zahraniční a kulturní rubriku
         System.out.println("\nŘÁDKY");
         System.out.println("- HERMES - " + a + " řádků je " + pocetZnaku + " znaků ve Wordu (což je " + pocetNormostran + " normostran)");
         System.out.println("- ROZHOVOR - " + a + " řádků je " + pocetOtazekRadku + " otázek s odpověďmi (+ extra perex na 240 znaků)");
 
+        // Výpis přepočtených řádků, znaků a otázek pro sobotní kritickou přílohu Orientace LN
         System.out.println("\nORIENTACE LN");
         System.out.println("- MS WORD - " + a + " znaků je " + pocetRadkuOrientace + " řádků v Hermesu");
         System.out.println("- HERMES - " + a + " řádků je " + pocetZnakuOrientace + " znaků ve Wordu (což je " + pocetNormostran + " normostran)");
